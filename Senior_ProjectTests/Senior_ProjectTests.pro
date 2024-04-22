bf47e8cf1cdd3dd743fafae4025e3c937faa0bd4
DeveshP <73601434+Devesh-PatelUF@users.noreply.github.com>
2024-04-22 16:18:03 -0400

    Add files via upload

--- Senior_ProjectTests/Senior_ProjectTests.pro
+++ Senior_ProjectTests/Senior_ProjectTests.pro
@@ -1,7 +1,7 @@
 QT += testlib
 QT += gui
 QT       += core texttospeech
-QT    += widgets webenginewidgets
+QT    += widgets
 CONFIG += qt warn_on depend_includepath testcase
 
 TEMPLATE = app
--- Senior_ProjectTests/tst_mainwindowtest.cpp
+++ Senior_ProjectTests/tst_mainwindowtest.cpp
@@ -1,4 +1,5 @@
 #include <QtTest>
+#include <QTextStream>
 #include "../Senior_Project/mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -18,13 +19,26 @@ private slots:
 
 
 
-    //testing size, position
+    //testing sizes of the UI Elements
     void test_capture_button_properties();
     void test_recapture_button_properties();
     void test_continue_button_properties();
+    void test_combo_box_properties();
+    void test_combo_box2_properties();
+    void test_reloadButton_properties();
+    void test_reloadButton2_properties();
+    void test_reloadButton3_properties();
+    void test_loadingText_properties();
+    void test_loadingWindow_properties();
+    void test_textEdit_properties();
+    void test_objectNameLabel_properties();
+    void test_progressBar_properties();
+    void test_imageLabel_properties();
+    void test_defintionLabel_properties();
 
 
-    //testing all behavior when a button is clicked. i.e. does it change size of other stuff, does it hide other stuff, does it change text
+
+    //testing behavior when a button is clicked to see if the correct UI Elements are present
     void test_capture_button_click();
     void test_recapture_button_click();
     void test_continue_button_click();
@@ -58,24 +72,102 @@ void mainWindowTest::test_capture_button_properties()
     Ui::MainWindow* ui = mainWindow->getUi();
     QVERIFY(ui->captureButton->height() == 115);
     QVERIFY(ui->captureButton->width() == 446);
-    QVERIFY(ui->captureButton->x() == 410);
-    QVERIFY(ui->captureButton->y() == 730);
 }
-void mainWindowTest::test_recapture_button_properties() \
+void mainWindowTest::test_recapture_button_properties()
 {
     Ui::MainWindow* ui = mainWindow->getUi();
     QVERIFY(ui->recaptureButton->height() == 115);
     QVERIFY(ui->recaptureButton->width() == 446);
-    QVERIFY(ui->recaptureButton->x() == 110);
-    QVERIFY(ui->recaptureButton->y() == 730);
 }
 void mainWindowTest::test_continue_button_properties()
 {
     Ui::MainWindow* ui = mainWindow->getUi();
     QVERIFY(ui->continueButton->height() == 115);
     QVERIFY(ui->continueButton->width() == 446);
-    QVERIFY(ui->continueButton->x() == 710);
-    QVERIFY(ui->continueButton->y() == 730);
+}
+
+void mainWindowTest::test_combo_box_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->comboBox->height() == 41);
+    QVERIFY(ui->comboBox->width() == 161);
+}
+
+void mainWindowTest::test_combo_box2_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->comboBox_2->height() == 41);
+    QVERIFY(ui->comboBox_2->width() == 231);
+}
+
+void mainWindowTest::test_reloadButton_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->reloadButton->height() == 51);
+    QVERIFY(ui->reloadButton->width() == 51);
+}
+
+void mainWindowTest::test_reloadButton2_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->reloadButton_2->height() == 51);
+    QVERIFY(ui->reloadButton_2->width() == 51);
+}
+
+void mainWindowTest::test_reloadButton3_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->reloadButton_3->height() == 50);
+    QVERIFY(ui->reloadButton_3->width() == 51);
+}
+
+void mainWindowTest::test_loadingText_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->loadingText->height() == 51);
+    QVERIFY(ui->loadingText->width() == 341);
+}
+
+void mainWindowTest::test_loadingWindow_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->loadingWindow->height() == 121);
+    QVERIFY(ui->loadingWindow->width() == 361);
+}
+
+void mainWindowTest::test_textEdit_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->textEdit->height() == 51);
+    QVERIFY(ui->textEdit->width() == 361);
+}
+
+void mainWindowTest::test_objectNameLabel_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->objectNameLabel->height() == 51);
+    QVERIFY(ui->objectNameLabel->width() == 821);
+}
+
+void mainWindowTest::test_progressBar_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->progressBar->height() == 31);
+    QVERIFY(ui->progressBar->width() == 361);
+}
+
+void mainWindowTest::test_imageLabel_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->imageLabel->height() == 600);
+    QVERIFY(ui->imageLabel->width() == 800);
+}
+
+void mainWindowTest::test_defintionLabel_properties()
+{
+    Ui::MainWindow* ui = mainWindow->getUi();
+    QVERIFY(ui->definitionLabel->height() == 111);
+    QVERIFY(ui->definitionLabel->width() == 801);
 }
 
 
@@ -84,56 +176,82 @@ void mainWindowTest::test_capture_button_click()
     Ui::MainWindow* ui = mainWindow->getUi();
     QTest::mouseClick(ui->captureButton, Qt::LeftButton);
 
-
+    QVERIFY(ui->imageLabel->isVisible());
     QVERIFY(ui->objectNameLabel->isVisible());
     QVERIFY(ui->recaptureButton->isVisible());
     QVERIFY(ui->continueButton->isVisible());
     QVERIFY(!(ui->captureButton->isVisible()));
+    QVERIFY(ui->comboBox->isVisible());
+    QVERIFY(ui->comboBox_2->isVisible());
+    QVERIFY(ui->reloadButton_3->isVisible());
+    QVERIFY(!(ui->reloadButton->isVisible()));
+    QVERIFY(!(ui->reloadButton_2->isVisible()));
     QVERIFY(ui->objectNameLabel->text().compare("Is this picture correct?")==0);
 
 
-
-
 }
+
+
 void mainWindowTest::test_recapture_button_click() {
     Ui::MainWindow* ui = mainWindow->getUi();
     QTest::mouseClick(ui->captureButton, Qt::LeftButton);
     QTest::mouseClick(ui->recaptureButton, Qt::LeftButton);
-    QVERIFY(!ui->objectNameLabel->isVisible());
+    QVERIFY(ui->imageLabel->isVisible());
+    QVERIFY(ui->objectNameLabel->isVisible());
     QVERIFY(ui->captureButton->isVisible());
-    QVERIFY(ui->objectNameLabel->text().compare("Take another picture!")==0);
+    QVERIFY(ui->comboBox->isVisible());
+    QVERIFY(ui->comboBox_2->isVisible());
+    QVERIFY(ui->reloadButton->isVisible());
+    QVERIFY(ui->reloadButton_2->isVisible());
+    QVERIFY(ui->reloadButton_3->isVisible());
+    QVERIFY(ui->objectNameLabel->text().compare("Object Identifier")==0);
 }
 
 void mainWindowTest::test_continue_button_click() {
     Ui::MainWindow* ui = mainWindow->getUi();
     QTest::mouseClick(ui->captureButton, Qt::LeftButton);
     QTest::mouseClick(ui->continueButton, Qt::LeftButton);
+    QVERIFY(ui->imageLabel->isVisible());
     QVERIFY(ui->objectNameLabel->isVisible());
     QVERIFY(ui->definitionLabel->isVisible());
-    QVERIFY(ui->objectNameLabel->text().compare("Object")==0);
-    QVERIFY(ui->definitionLabel->text().compare("Definition")==0);
     QVERIFY(ui->recaptureButton->isVisible());
     QVERIFY(ui->continueButton->isVisible());
+    QVERIFY(!(ui->comboBox->isVisible()));
+    QVERIFY(ui->comboBox_2->isVisible());
+    QVERIFY(ui->reloadButton_3->isVisible());
+    QVERIFY(!(ui->reloadButton->isVisible()));
+    QVERIFY(!(ui->reloadButton_2->isVisible()));
 
 }
 void mainWindowTest::test_newObject_button_click() {
     Ui::MainWindow* ui = mainWindow->getUi();
-    QTest::mouseClick(ui->captureButton, Qt::LeftButton);
     QTest::mouseClick(ui->continueButton, Qt::LeftButton);
     QTest::mouseClick(ui->recaptureButton, Qt::LeftButton);
-    QVERIFY(!ui->objectNameLabel->isVisible());
+    QVERIFY(ui->imageLabel->isVisible());
+    QVERIFY(ui->objectNameLabel->isVisible());
     QVERIFY(ui->captureButton->isVisible());
-    QVERIFY(ui->objectNameLabel->text().compare("Take another picture!")==0);
+    QVERIFY(ui->captureButton->isVisible());
+    QVERIFY(ui->comboBox->isVisible());
+    QVERIFY(ui->comboBox_2->isVisible());
+    QVERIFY(ui->reloadButton->isVisible());
+    QVERIFY(ui->reloadButton_2->isVisible());
+    QVERIFY(ui->reloadButton_3->isVisible());
+    QVERIFY(ui->objectNameLabel->text().compare("Object Identifier")==0);
 }
 void mainWindowTest::test_replayAudio_button_click() {
     Ui::MainWindow* ui = mainWindow->getUi();
     QTest::mouseClick(ui->captureButton, Qt::LeftButton);
     QTest::mouseClick(ui->continueButton, Qt::LeftButton);
-    QTest::mouseClick(ui->continueButton, Qt::LeftButton);
+    QVERIFY(ui->imageLabel->isVisible());
     QVERIFY(ui->objectNameLabel->isVisible());
     QVERIFY(ui->definitionLabel->isVisible());
-    QVERIFY(ui->objectNameLabel->text().compare("Object")==0);
-    QVERIFY(ui->definitionLabel->text().compare("Definition")==0);
+    QVERIFY(ui->recaptureButton->isVisible());
+    QVERIFY(ui->continueButton->isVisible());
+    QVERIFY(!(ui->comboBox->isVisible()));
+    QVERIFY(ui->comboBox_2->isVisible());
+    QVERIFY(ui->reloadButton_3->isVisible());
+    QVERIFY(!(ui->reloadButton->isVisible()));
+    QVERIFY(!(ui->reloadButton_2->isVisible()));
 }
 
 QTEST_MAIN(mainWindowTest)
